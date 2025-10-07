eb1376c042185624911ea5e15f6a13a6cc6e8086
Johan <s1212230@student.windesheim.nl>
2025-10-07 12:04:50 +0200

    Alle classes en interfaces in nieuwe bestanden aangemaakt voor het toevoegen van productcategoriën

---
+++ Grocery.Core/Interfaces/Services/IProductCategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Interfaces.Services
+{
+    public interface IProductCategoryService
+    {
+        public ProductCategory Add(ProductCategory item);
+        public List<ProductCategory> GetAll();
+        public List<ProductCategory> GetAllOnCategoryId(int id);
+    }
+}
---
+++ Grocery.Core/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Grocery.Core.Models
+{
+    public class ProductCategory : Model 
+    {
+        public int CategoryId { get; set; }
+        public int ProductId { get; set; }  
+        public ProductCategory(int id, int CategoryId, int ProductId) : base(id, "")
+        {
+            this.CategoryId = CategoryId;
+            this.ProductId = ProductId;
+        }
+
+        public Product? Product { get; set; }
+        public Category? Category { get; set; }
+    }
+}
